566a42d4a57ebe0d24fb7d697b424ec916f19f02
Haroon <haroon.ali.1104@gmail.com>
2024-01-18 22:00:25 +0000

    look for uploaded file in new issue comment

--- netlify/functions/pd-grammar-bot.js
+++ netlify/functions/pd-grammar-bot.js
@@ -3,7 +3,9 @@ import { createLambdaFunction, createProbot } from '@probot/adapter-aws-lambda-s
 const appFn = (app) => {
     app.on("issue_comment.created", async (context) => {
         if (context.payload.comment.user.type !== 'Bot') {
-            
+            if (context.payload.comment.body.includes('[pd.pdf]')) {
+                console.log('There is a PDF.');
+            }
         }
     });
 };
