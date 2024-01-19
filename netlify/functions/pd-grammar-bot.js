1b238d1bbc0a4cf28ea0bcd1b674ed2a9a55593b
Haroon <haroon.ali.1104@gmail.com>
2024-01-19 19:16:40 +0000

    get PDF from url

--- netlify/functions/pd-grammar-bot.js
+++ netlify/functions/pd-grammar-bot.js
@@ -1,15 +1,41 @@
-import { createLambdaFunction, createProbot } from '@probot/adapter-aws-lambda-serverless';
+const { createLambdaFunction, createProbot } = require('@probot/adapter-aws-lambda-serverless');
+const pdf = require('pdf-parse');
 
 const appFn = (app) => {
     app.on("issue_comment.created", async (context) => {
         if (context.payload.comment.user.type !== 'Bot') {
             if (context.payload.comment.body.includes('[pd.pdf]')) {
-                console.log('There is a PDF.');
+                const match = context.payload.comment.body.match(/\[pd\.pdf\]\(([^)]+)\)/);
+                const pdfUrl = match ? match[1] : null;
+
+                console.log(pdfUrl);
+
+                let res = await fetch(pdfUrl);
+                const fileBuffer = await res.arrayBuffer();
+
+                
             }
         }
     });
 };
 
-export const handler = createLambdaFunction(appFn, {
+module.exports.handler = createLambdaFunction(appFn, {
     probot: createProbot(),
 });
+
+function renderPage(pageData) {
+    return pageData.getTextContent()
+        .then(textContent => {
+            let lastY, text = '';
+            for (let item of textContent.items) {
+                if (lastY == item.transform[5] || !lastY) {
+                    text += item.str + ' ';
+                }
+                else {
+                    text += '\n' + item.str + ' ';
+                }
+                lastY = item.transform[5];
+            }
+            return text;
+        });
+}
