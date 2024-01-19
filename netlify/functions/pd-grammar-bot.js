923ea90821cb478641bc942c587c060b6debfc38
Haroon <haroon.ali.1104@gmail.com>
2024-01-19 20:29:26 +0000

    create comment with count of total mistakes

--- netlify/functions/pd-grammar-bot.js
+++ netlify/functions/pd-grammar-bot.js
@@ -5,18 +5,38 @@ const appFn = (app) => {
     app.on("issue_comment.created", async (context) => {
         if (context.payload.comment.user.type !== 'Bot') {
             if (context.payload.comment.body.includes('[pd.pdf]')) {
-                const match = context.payload.comment.body.match(/\[pd\.pdf\]\(([^)]+)\)/);
-                const pdfUrl = match ? match[1] : null;
-
-                console.log(pdfUrl);
-
-                let res = await fetch(pdfUrl);
-                const fileBuffer = await res.arrayBuffer();
-
-                const pdfData = await pdf(fileBuffer, { pagerender: renderPage });
-                const pdfText = pdfData.text;
-
-                console.log(pdfText);
+                try {
+                    const match = context.payload.comment.body.match(/\[pd\.pdf\]\(([^)]+)\)/);
+                    const pdfUrl = match ? match[1] : null;
+    
+                    let res = await fetch(pdfUrl);
+                    const fileBuffer = await res.arrayBuffer();
+    
+                    const pdfData = await pdf(fileBuffer, { pagerender: renderPage });
+    
+                    const langToolUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:8010/v2/check' : process.env.LANG_TOOL_URL;
+    
+                    res = await fetch(langToolUrl, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+                        },
+                        body: new URLSearchParams({
+                            language: 'en-GB',
+                            text: pdfData.text
+                        })
+                    });
+    
+                    const data = await res.json();
+    
+                    const commentBody = `Total mistakes: ${data.matches.length}`;
+    
+                    const params = context.issue({ body: commentBody });
+    
+                    return context.octokit.issues.createComment(params);
+                } catch (error) {
+                    console.log(error.message);
+                }
             }
         }
     });
