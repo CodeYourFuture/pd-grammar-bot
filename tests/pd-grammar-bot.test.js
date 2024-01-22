33434503bbcc646242b0ced048a661a72b46d162
Haroon <haroon.ali.1104@gmail.com>
2024-01-22 18:29:39 +0000

    create GitHub event for testing

--- tests/pd-grammar-bot.test.js
+++ tests/pd-grammar-bot.test.js
@@ -1,20 +1,30 @@
-const { privateKey } = require('./dummy-private-key.json');
-
+const { privateKey } = require('./sample-data/dummy-private-key.json');
 process.env.APP_ID = 1;
 process.env.PRIVATE_KEY = privateKey;
 process.env.WEBHOOK_SECRET = 'webhookSecret';
 
+const crypto = require('crypto');
 const { handler } = require('../netlify/functions/pd-grammar-bot');
 const payload = require('../sample-data/github-event.json');
 
 describe('GitHub app', () => {
     it('works', async () => {
-        const result = await handler({
-            httpMethod: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(payload)
-        });
+        const result = await handler(createGitHubRequest(payload));
     });
 });
+
+function createGitHubRequest(data) {
+    const body = JSON.stringify(data);
+    const hmac = crypto.createHmac('sha256', 'webhookSecret');
+    const signature = 'sha256=' + hmac.update(body).digest('hex');
+
+    return {
+        httpMethod: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'X-Hub-Signature-256': signature,
+            'X-GitHub-Event': 'issue_comment'
+        },
+        body
+    };
+};
