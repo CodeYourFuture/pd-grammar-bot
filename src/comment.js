6e3deac292f0e9189aa1d5bef4dc1acd754d527b
Haroon <haroon.ali.1104@gmail.com>
2024-02-06 18:39:09 +0500

    add comment text for each mistake

--- src/comment.js
+++ src/comment.js
@@ -3,7 +3,24 @@ function createCommentBody(langToolResult) {
         const commentBody = `### Total Mistakes: ${langToolResult.matches.length}`;
         return commentBody;
     } else {
-        
+        let commentBody = `### Total Mistakes: ${langToolResult.matches.length}`;
+
+        for (let mistake of langToolResult.matches) {
+            let replacements = [];
+            for (let replacement of mistake.replacements) {
+                replacements.push(replacement.value);
+            }
+
+            commentBody += "\n";
+            commentBody += `> **What:** ${mistake.message}`;
+            commentBody += "\n";
+            commentBody += `> **Where:** ${mistake.context.text}`;
+            commentBody += "\n";
+            commentBody += `> **Replacements:** ${replacements.join(", ")}`;
+            commentBody += "\n";
+        }
+
+        return commentBody;
     }
 }
 
