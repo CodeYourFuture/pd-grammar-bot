ebc2b94dd2054ca2239c8da67c5df1702174a208
Haroon <haroon.ali.1104@gmail.com>
2024-03-19 20:03:45 +0000

    finalize comment body

--- netlify/functions/pd-grammar-bot.js
+++ netlify/functions/pd-grammar-bot.js
@@ -1,15 +1,15 @@
 const { createLambdaFunction, createProbot } = require('@probot/adapter-aws-lambda-serverless');
-const { getPdfText } = require('../../src/pdf');
+const { getText } = require('../../src/text');
 const { checkText } = require('../../src/langTool');
 const { createCommentBody } = require('../../src/comment');
 
 const appFn = (app) => {
     app.on("issue_comment.created", async (context) => {
         if (context.payload.comment.user.type === 'User') {
-            if (/\.pdf\b/.test(context.payload.comment.body)) {
+            if (/\.docx\b/.test(context.payload.comment.body)) {
                 try {
-                    const pdfText = await getPdfText(context.payload.comment.body);
-                    const langToolResult = await checkText(pdfText);
+                    const text = await getText(context.payload.comment.body);
+                    const langToolResult = await checkText(text);
 
                     const commentBody = createCommentBody(langToolResult);
                     const params = context.issue({ body: commentBody });
--- src/comment.js
+++ src/comment.js
@@ -1,29 +1,59 @@
+let langToolResult = require("../sample-data/languagetool-response.json");
+
 function createCommentBody(langToolResult) {
-    if (langToolResult.matches.length === 0) {
-        const commentBody = `### Total Mistakes Identified: ${langToolResult.matches.length}`;
-        return commentBody;
-    } else {
-        let commentBody = `### Total Mistakes Identified: ${langToolResult.matches.length}`;
+    const mistakesCount = langToolResult.matches.length;
+    const categoryMatches = getCategoryMatches(langToolResult);
+    const commentBody = generateCommentBody(categoryMatches, mistakesCount);
 
-        for (let mistake of langToolResult.matches) {
-            let replacements = [];
-            mistake.replacements.forEach((e, i) => {
-                if (i <= 3) {
-                    replacements.push(e.value);
-                }
-            });
+    return commentBody;
+}
 
-            commentBody += "\n";
-            commentBody += `> **What:** ${mistake.message}`;
-            commentBody += "\n";
-            commentBody += `> **Where:** ${mistake.context.text}`;
-            commentBody += "\n";
-            commentBody += `> **Replacements:** ${replacements.join(", ")}`;
-            commentBody += "\n";
+function getCategoryMatches(langToolResult) {
+    const categoryMatches = {};
+
+    for (let match of langToolResult.matches) {
+        const matchProperties = {
+            message: match.message,
+            context: match.context.text
+        };
+
+        if (match.rule.category.name in categoryMatches) {
+            categoryMatches[match.rule.category.name].push(matchProperties);
+        } else {
+            categoryMatches[match.rule.category.name] = [];
+            categoryMatches[match.rule.category.name].push(matchProperties);
+        }
+    }
+
+    return categoryMatches;
+}
+
+function generateCommentBody(categoryMatches, mistakesCount) {
+    let commentBody = `### ${mistakesCount >= 3 ? "👎" : "👍"} Total Possible Mistakes: ${mistakesCount}`;
+
+    if (mistakesCount > 0) {
+        commentBody += "\n";
+
+        for (let key in categoryMatches) {
+            commentBody += `\n- ${key}: ${categoryMatches[key].length}`;
         }
 
-        return commentBody;
+        for (let key in categoryMatches) {
+            commentBody += "\n\n";
+            commentBody += `#### ${key}`;
+
+            categoryMatches[key].forEach((e, i) => {
+                if (i < 3) {
+                    commentBody += `\n\n> **What:** ${e.message}`;
+                    commentBody += `\n> **Where:** ${e.context}`;
+                }
+            });
+        }
+
+        commentBody += `\n\n#### Use a Grammar AI to fix these mistakes.`;
     }
+
+    return commentBody;
 }
 
 module.exports.createCommentBody = createCommentBody;
